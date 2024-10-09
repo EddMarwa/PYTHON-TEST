299c158393fc78330976dd13ff65ac71d4ec4e92
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-10-10 02:04:02 +0300

    Done Quiz 2

--- find_max.py
+++ find_max.py
@@ -1,11 +1,10 @@
-def find_max(num1, num2):
+def maxnum(num1, num2):
     if num1 > num2:
         return num1
     else:
         return num2
+num1 = 3
+num2 = 7
+maximum = maxnum(num1, num2)
+print(f"The maximum number is: {maximum}")
 
-# Example usage:
-number1 = 10
-number2 = 20
-maximum = find_max(number1, number2)
-print(f"The maximum of {number1} and {number2} is {maximum}.")
