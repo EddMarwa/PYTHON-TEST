f4aa5eb2ae0d2d9d4ea5715e32d57e95b680cde7
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-10-10 00:22:30 +0300

    Tdays

---
+++ find_max.py
@@ -0,0 +1,11 @@
+def find_max(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+# Example usage:
+number1 = 10
+number2 = 20
+maximum = find_max(number1, number2)
+print(f"The maximum of {number1} and {number2} is {maximum}.")
